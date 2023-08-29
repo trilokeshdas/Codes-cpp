68214b565298d3543fa6d6074583c07181ce5368
trilokeshdas <124261689+trilokeshdas@users.noreply.github.com>
2023-08-29 23:34:05 +0530

    new

---
+++ OOPs/Constructor/constructor.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class complex
+{
+    int rl;
+    int im;
+public:
+      complex()
+      {
+        cout<<"enter the real part"<<endl;
+        cin>>rl;
+        cout<<"enter the imaginary part"<<endl;
+        cin>>im;
+      }
+      void display()
+      {
+        cout<<"the complex number is "<<rl<<" + "<<im<<"i"<<endl;
+      }
+};
+
+
+int main(){
+    complex c1,c2;
+    c1.display();
+    c2.display();
+    return 0;
+}
